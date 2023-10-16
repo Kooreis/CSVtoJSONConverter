6e85ca78f8b9e761a823b2b70a68c777ca6febac
Kooreis <kooreis.official@gmail.com>
2023-10-15 19:52:12 -0500

    Defines the main class and method, initializes necessary variables for CSV file reading and JSON array creation.

--- CSV_to_JSON_Converter.java
+++ CSV_to_JSON_Converter.java
@@ -1,8 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+public class CSVtoJSONConverter {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        List<String> headers = new ArrayList<String>();
+        JSONArray jsonArray = new JSONArray();
+        String csvFile = args[0];
+        String line = "";
+        String cvsSplitBy = ",";
+        int lineCounter = 0;
